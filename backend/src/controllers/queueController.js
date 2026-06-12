0e005a37c7167b2d0836cad303a220c19f51f5d2
Hazem <abdelazez.hazem@gmail.com>
2026-06-12 20:36:43 +0300

    api that retrive all Activ queues

--- backend/src/controllers/queueController.js
+++ backend/src/controllers/queueController.js
@@ -42,5 +42,84 @@ const createQueue = async (req, res) => {
   }
 };
 
-module.exports = { createQueue };
 
+
+
+
+// get all active queues for a clinic
+const getActiveQueues = async (req, res) => {
+  try {
+    const { clinicId } = req.params;
+
+    if (!clinicId) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Clinic ID is required',
+      });
+    }
+    const activeQueues = await Queue.find({ clinicId, isActive: true });
+
+    return res.status(200).json({
+      status: 'success',
+      results: activeQueues.length,
+      data: {
+        queues: activeQueues,
+      },
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong on the server',
+      error: err.message, 
+    });
+  }
+};
+
+
+
+// close a specific queue (Update isActive to false)
+const closeQueue = async (req, res) => {
+  try {
+    const { queueId } = req.params;
+
+    if (!queueId) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Queue ID is required',
+      });
+    }
+    const updatedQueue = await Queue.findByIdAndUpdate(
+      queueId,
+      { isActive: false },
+      { new: true }
+    );
+
+    if (!updatedQueue) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'No queue found with that ID',
+      });
+    }
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Queue has been closed successfully',
+      data: {
+        queue: updatedQueue,
+      },
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong while closing the queue',
+      error: err.message,
+    });
+  }
+};
+
+
+// Exporting the functions to be used in routes
+
+module.exports = { createQueue, getActiveQueues , closeQueue };
