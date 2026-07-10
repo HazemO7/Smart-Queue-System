67ea5e5ef30a298f15dd397836860a93d7771e24
Hazem <abdelazez.hazem@gmail.com>
2026-07-10 20:26:42 +0300

    update app.jsx with routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import './styles/components.css';
+import App from './App.jsx';
+import { LanguageProvider } from './context/LanguageContext';
+import { AuthProvider } from './context/AuthContext';
+import { SocketProvider } from './context/SocketContext';
+import { QueueProvider } from './context/QueueContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <LanguageProvider>
+        <AuthProvider>
+          <SocketProvider>
+            <QueueProvider>
+              <App />
+            </QueueProvider>
+          </SocketProvider>
+        </AuthProvider>
+      </LanguageProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
